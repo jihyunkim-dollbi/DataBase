6fe7c3d029a1f94a4e8da0ea6435bba8f97edc3f
sist <sist@DESKTOP-RVDJDNC>
2020-02-19 17:43:25 +0900

    2020 02 19

--- FoodPROJECT/src/com/sist/dao/EmpDAO.java
+++ FoodPROJECT/src/com/sist/dao/EmpDAO.java
@@ -28,11 +28,51 @@ public class EmpDAO {
 	public void getConnection(){
 		
 		try
-		{
-			Context init=new InitialContext();
-			Context c= (Context)init.lookup("java://env/comp");
+		{	
+			//탐색기를 연다!
+			Context init=new InitialContext(); // JNDI Registry(에서 커넥션 주소를 가지고 있음!가상 주소 저장 공간!)(JAVA NAMING DIRECTORY INTERFACE!)
+			
+			/*주소가 저장되어있는 형식이 폴더형식!
+			 * "java://env/comp" => C DRIVE의 이름!
+			 * C드라이브를  URL형태로 만듬! 아무도 모르는 주소를 만들기 위해!
+			 * C드라이브안에 이 jdbc/oracle 이름으로 저장된 정보를 달라! 
+			 * type="javax.sql.DataSource"를 준다!!
+			 * 
+			 * 5개의 커넥션주소값에 이름을 주어서 찾아옴!  	DataSource ds=(DataSource)c.lookup("jdbc/oracle");
+			 * 연결하는 객체를 달라! conn=ds.getConnection();
+			 */
+			
+			/* 
+			 * dbcp방식을 만들수도 있다!!
+			 * Connection[] conn={100,200,300,400,500};
+			 * boolean[] sw={false, false,false,false,false}
+			 * 
+			 * if(요청.equals("jdbc/oracle");
+			 * 	{
+			 * 		for(int i=0;i<5; i++)
+			 * 		{
+			 * 			return conn[i];
+			 * 			break; 
+			 * 		}
+			 * 	}
+			 * 
+			 */
+			
+			//저장된 폴더위치로 접근 - 고정주소
+			Context c= (Context)init.lookup("java://comp//env");// => JNDI
+			
+			//**lookup => setLookup("별칭",실제클래스주소)
+			// 이름으로 클래스 주소를 찾아옴!
+			
+			//실제 커넥션 주소 요청 ==> 오라클 정보와 관련된 모든 정보를 전송해줌(DataSource가 모두 가짐!) - 우리가 만든 주소
 			DataSource ds=(DataSource)c.lookup("jdbc/oracle");
+			
+			//모든 정보를 가진  ds의 주소값을 받는다!!
 			conn=ds.getConnection();
+			
+			//5개의 커넥션을 생성했고 5개의 커넥션 주소가 만들어졌을 것이다.
+			//false & true : false면 반환된 상태, true면 사용중!
+		
 			
 		}catch(Exception ex) {}
 		
@@ -58,7 +98,57 @@ public class EmpDAO {
 	}
 	
 	//기능
-	
+	public ArrayList<EmpVO> empAllData()
+	{
+		ArrayList<EmpVO> list=new ArrayList<EmpVO>();
+		
+		try
+		{
+			//사용할 커넥션 주소를 얻어옴!
+			getConnection();
+			/*
+			String sql="SELECT empno, ename, job, hiredate, sal, dname, loc "
+			    	  +"FROM emp, dept "
+					  +"WHERE emp.deptno=dept.deptno";
+			*/
+			String sql="SELECT * FROM emp_dept";
+					
+					
+			ps=conn.prepareStatement(sql);
+			ResultSet rs=ps.executeQuery();
+			
+			
+			while(rs.next())
+			{
+				EmpVO vo=new EmpVO();
+				vo.setEmpno(rs.getInt(1));
+				vo.setEname(rs.getString(2));
+				vo.setJob(rs.getString(3));
+				vo.setHiredate(rs.getDate(4));
+				vo.setSal(rs.getInt(5));
+				vo.getDvo().setDname(rs.getString(6));
+				vo.getDvo().setLoc(rs.getString(7));
+				
+				list.add(vo);
+				
+				
+			}
+			rs.close();
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			
+		}
+		finally
+		{
+			//반환
+			disConnection();
+			
+		}
+		return list;
+		
+	}
 	
 	
 	
--- FoodPROJECT/src/com/sist/view/MainServlet.java
+++ FoodPROJECT/src/com/sist/view/MainServlet.java
@@ -7,7 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import java.util.*;
+import com.sist.dao.*;
 
 @WebServlet("/MainServlet")
 public class MainServlet extends HttpServlet {
@@ -16,7 +17,62 @@ public class MainServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		//dbcp연습!!
-		//
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out=response.getWriter();
+		
+		EmpDAO dao=new EmpDAO();
+		ArrayList<EmpVO> list=dao.empAllData();
+		
+		//list에 이쓴 데이터 출력
+
+		out.println("<html>");
+		out.println("<head>");
+		/*
+		out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+		out.println("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">");
+		out.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>");
+		out.println("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\"></script>");
+			*/
+		
+		out.println("</head>");
+		out.println("<center>");
+		out.println("<h1>사원목록</h1>");
+		out.println("<table class=\"table table-hover\" border=1 width=800>");
+		out.println("<tr>");
+		
+		out.println("<th>사번</th>");
+		out.println("<th>이름</th>");
+		out.println("<th>직위</th>");
+		out.println("<th>입사일</th>");
+		out.println("<th>급여</th>");
+		out.println("<th>부서명</th>");
+		out.println("<th>근무지</th>");
+	
+		out.println("</tr>");
+		
+		for(EmpVO vo:list)
+		{
+			out.println("<tr>");
+			
+			out.println("<td>"+vo.getEmpno()+"</td>");
+			out.println("<td>"+vo.getEname()+"</td>");
+			out.println("<td>"+vo.getJob()+"</td>");
+			out.println("<td>"+vo.getHiredate()+"</td>");
+			out.println("<td>"+vo.getSal()+"</td>");
+			out.println("<td>"+vo.getDvo().getDname()+"</td>");
+			out.println("<td>"+vo.getDvo().getLoc()+"</td>");
+		
+			out.println("</tr>");
+			
+		}
+		
+		out.println("</table>");
+		out.println("</center>");
+		out.println("</body>");
+		out.println("</html>");
+		
+				
+		
 		
 		
 		
