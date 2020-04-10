2b274dd2fbae63ea76a2001b9f512609360c1f6d
jihyunkim-dollbi <dollbi5@gmail.com>
2020-04-10 19:32:32 +0900

    2020 04 10

--- MovieProject2/src/com/sist/dao/MainClass.java
+++ MovieProject2/src/com/sist/dao/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			
-		// 도수창에서 띄워보려 메인클래스 만듬
+		// 도수창에서 띄워보려 메인클래스  만듬
 		
 		MovieDAO dao=MovieDAO.newInstance();
 		//20번까지 현재상영 영화
